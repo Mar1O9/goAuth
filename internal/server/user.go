e738e6fca5e7726c718f458e214f5992090066d7
Mar1O9 <mahmud.yousif.04@gmail.com>
2025-04-16 21:49:58 +0200

    some updates

--- internal/server/user.go
+++ internal/server/user.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Mahmoud Abdelrahman <mahmud.yousif.04@gmail.com>
+// Copyright 2025 Mahmoud Abdelrahman <deprecated>
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
